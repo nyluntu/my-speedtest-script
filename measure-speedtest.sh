94087250e46379b23c71c267849091386a9c8ed9
Turo Nylund <turo.nylund@outlook.com>
2019-03-20 18:02:30 +0200

    update script paths

--- measure-speedtest.sh
+++ measure-speedtest.sh
@@ -3,6 +3,9 @@
 # this script needs speedtest-cli tool to work.
 # https://github.com/sivel/speedtest-cli
 
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
+
 # create all output files
 mkdir -p output/elisa
 mkdir -p output/dna
@@ -14,19 +17,19 @@ echo "Output directories created"
 
 # call speedtest tool and save output
 echo "$(date): Elisa start"
-/mnt/c/dev/speedtests/speedtest-cli --server 4549 --json --bytes > output/elisa/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 4549 --json --bytes > output/elisa/"$(date +"%F_%H-%M").json"
 
 echo "$(date): Dna start"
-/mnt/c/dev/speedtests/speedtest-cli --server 4427 --json --bytes > output/dna/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 4427 --json --bytes > output/dna/"$(date +"%F_%H-%M").json"
 
 echo "$(date): Nebula start"
-/mnt/c/dev/speedtests/speedtest-cli --server 864 --json --bytes > output/nebula/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 864 --json --bytes > output/nebula/"$(date +"%F_%H-%M").json"
 
 echo "$(date): Vincit start"
-/mnt/c/dev/speedtests/speedtest-cli --server 8185 --json --bytes > output/vincit/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 8185 --json --bytes > output/vincit/"$(date +"%F_%H-%M").json"
 
 echo "$(date): Telia start"
-/mnt/c/dev/speedtests/speedtest-cli --server 10106 --json --bytes > output/telia/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 10106 --json --bytes > output/telia/"$(date +"%F_%H-%M").json"
 
 echo "$(date): Sigmatic start"
-/mnt/c/dev/speedtests/speedtest-cli --server 10413 --json --bytes > output/sigmatic/"$(date +"%F_%H-%M").json"
+${BASEDIR}/speedtest-cli --server 10413 --json --bytes > output/sigmatic/"$(date +"%F_%H-%M").json"
